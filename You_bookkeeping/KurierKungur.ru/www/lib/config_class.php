39c84c7d17fb507768ece92432de0882f5e97a26
web-del-master <37731445+web-del-master@users.noreply.github.com>
2019-01-08 21:55:43 +0500

    Add files via upload

---
+++ You_bookkeeping/KurierKungur.ru/www/lib/config_class.php
@@ -0,0 +1,16 @@
+<?php
+class Config{
+	  public $sitename = "kurierkungur.ru";
+	  public $address = "http://kurierkungur.ru";
+	  public $db_host = "localhost";
+	  public $db_user = "root@localhost";
+	  public $db_password = "";
+	  public $db_name = "kurier_kungur";
+	  public $db_prefix = "kurier_";
+	  public $sym_query = "{?}";
+	  
+	  public $admname = "Кузнецов Сергей";
+	 
+}  
+
+?>
