43b01d7b54688d8367c4b34f44253db3ab9490c0
web-del-master <37731445+web-del-master@users.noreply.github.com>
2020-07-21 18:47:56 +0500

    Add files via upload

--- rus_lang/script.js
+++ rus_lang/script.js
@@ -1,6 +1,32 @@
 window.onload = function (){
-    let pay_descr = document.querySelectorAll('.pay_descr__card');
-    let btn_tab = document.querySelectorAll('.btn_tab');
+    let pay_descr = document.querySelectorAll('.pay_descr__card'),
+        btn_tab = document.querySelectorAll('.btn_tab');
+
+    let active_modal = document.querySelectorAll('.active_modal'),
+        modal_bg = document.querySelector('.modal_bg'),
+        modal_content = document.querySelector('.modal_content');
+        del = document.querySelector('.del');
+
+    modal_content.onclick = function(event){
+        if(event.target== del && !(modal_bg.classList.contains('hidden') && modal_content.classList.contains('hidden'))){
+            modal_bg.classList.add('hidden');
+            modal_content.classList.add('hidden'); 
+         }
+    };
+    modal_bg.onclick = function(e){
+        if(!(modal_bg.classList.contains('hidden') && modal_content.classList.contains('hidden'))){
+            modal_bg.classList.add('hidden');
+            modal_content.classList.add('hidden'); 
+         }  
+    };
+    for(let i=0; i< active_modal.length; i++){
+        active_modal[i].onclick = function(e){
+                 if(modal_bg.classList.contains('hidden') && modal_content.classList.contains('hidden')){
+                    modal_bg.classList.remove('hidden');
+                    modal_content.classList.remove('hidden'); 
+                 }       
+        }
+    }
 
     for(let i=0; i< btn_tab.length; i++){
         btn_tab[i].onclick = function(e){
